3cce825e7356a39adb5feba4571e6b56e9c9efb0
John Nycz <jnycz@758902.local>
2022-06-07 12:11:50 -0400

    Latest round

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,19 +7,19 @@ export default function Header({ title }) {
       <nav className="flex items-center justify-between flex-wrap py-3">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link href="/">
-              <a className="block mt-4 lg:inline-block lg:mt-0  hover:text-white mr-4"><span className="font-semibold text-xl tracking-tight">Candace Nycz<br/>Colorist</span></a>
-            </Link>
+            <a className="block mt-4 lg:inline-block lg:mt-0  hover:text-white mr-4"><span className="font-semibold text-xl tracking-tight">CANDACE NYCZ</span></a>
+        </Link>
+        </div>
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          Colorist
         </div>
         <div className="flex lg:flex lg:items-center lg:w-auto">
           <div className="flex  justify-end text-sm lg:flex-grow">
             <Link href="/about">
-              <a className="block mt-4 lg:inline-block lg:mt-0  hover:text-white mr-4">About</a>
-            </Link>
-            <Link href="/work" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
-            <a className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">Work</a>
+              <a className="block mt-4 lg:inline-block lg:mt-0  hover:text-white mr-4">about</a>
             </Link>
             <Link href="/contact" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white">
-            <a className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">Contact</a>
+              <a className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">contact</a>
             </Link>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import ReactPlayer from "react-player";
-
+//import ReactPlayer from "react-player";
+import ReactPlayer from "@components/ReactPlayer";
 import Header from "@components/Header";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
@@ -16,9 +16,9 @@ export default function Home() {
 
       <main>
         <Navbar />
-        <Header title="Reel" />
+        <Header title="color reel" />
         <div className="py-10 flex items-center justify-center">
-          <ReactPlayer url="https://vimeo.com/701093918" />  
+          <ReactPlayer url="https://vimeo.com/701093918" width="100%" height="100%" responsive="true" />  
         </div>
       </main>
 
