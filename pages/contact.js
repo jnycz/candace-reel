bd18f8d9e3a08096f0eeb481940fcd400f4f1477
john nycz <nycz88@gmail.com>
2023-11-14 14:50:00 -0500

    Added attribute to form for auto detection on platform

--- pages/contact.js
+++ pages/contact.js
@@ -24,6 +24,7 @@ export default function Contact() {
               method="POST" 
               data-netlify="true" 
               netlify-honeypot="bot-field"
+              netlify
             >
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">Name: <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" /></label>
