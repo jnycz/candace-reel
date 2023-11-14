d46166877524fa29b3c1b63409f51281ef93ac95
john nycz <nycz88@gmail.com>
2023-11-14 16:26:49 -0500

    Added new page for contact confirmaiton

--- pages/contact/thanks.js
+++ pages/contact/thanks.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
-import ReactPlayer from "@components/ReactPlayer";
 import Header from "@components/Header";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
