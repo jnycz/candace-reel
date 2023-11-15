86ac5d9dc1a06deda4cf82d0f0fdf9ce6dd9bba7
john nycz <nycz88@gmail.com>
2023-11-14 21:05:18 -0500

    Structured code

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -28,17 +28,25 @@ export default function Contact() {
             >
               <input name="bot-field" />
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Name: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" /></label>
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Name: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" />
+                </label>
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Company: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="company" /></label>
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Company: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="company" />
+                </label>
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Email: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" /></label>
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Email: <input required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" />
+                </label>
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Message: <textarea required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="message"></textarea></label>
-                </div>
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Message: <textarea required={true} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="message"></textarea>
+                </label>
+              </div>
               <div className="mb-4">
                 <button className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">send</button>
               </div>
